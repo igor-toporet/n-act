4eb961442c56511cfd1ac6213486ea7127eb6658
alex.davies <alex.davies@localhost>
2010-03-29 11:55:28 +0100

    Stop using stuff that only exists in c# 4

--- NAct/ThreaderWrapper.cs
+++ NAct/ThreaderWrapper.cs
@@ -13,12 +13,12 @@ namespace NAct
         /// <typeparam name="TInterface">The interface of the actor to create.</typeparam>
         /// <param name="creator">A function that creates a fresh instance of an object implementing TInterface.</param>
         /// <returns>An actor of type TInterface. The creator may not have run yet, but method calls on it will safely be queued for once it is created.</returns>
-        public static TInterface CreateActor<TInterface>(ObjectCreator<TInterface> creator) where TInterface : class, IActor
+        public static TInterface CreateActor<TInterface>(ObjectCreator<IActor> creator) where TInterface : class, IActor
         {
             CreatorInterceptor interceptor = new CreatorInterceptor(creator);
             return new ProxyGenerator().CreateInterfaceProxyWithoutTarget<TInterface>(interceptor);
         }
     }
 
-    public delegate T ObjectCreator<out T>();
+    public delegate T ObjectCreator<T>();
 }
